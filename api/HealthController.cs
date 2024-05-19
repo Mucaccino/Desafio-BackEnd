c73376e12055da1f954a91aaffd86aaf5dd5c379
Murillo L Do Carmo <murillodocarmo@gmail.com>
2024-05-18 21:03:03 -0300

    Add api /healthy

---
+++ api/HealthController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Motto.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CheckHealth()
+        {
+            // Aqui você pode adicionar lógica para verificar a saúde da sua aplicação.
+            // Por exemplo, você pode verificar conexões com bancos de dados, serviços externos, etc.
+            
+            // Se tudo estiver bem, você pode retornar um código 200 OK.
+            return Ok("Healthy");
+        }
+    }
+}
